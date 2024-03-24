3301cf86b4a949e5d101a4f198a0f72eb7a96173
s28094 <s28094@pjwstk.edu.pl>
2024-03-24 13:34:16 +0100

    Added RefrigeratedContainer.cs and implemented methods

---
+++ APBD_03/APBD_03/RefrigeratedContainer.cs
@@ -0,0 +1,34 @@
+﻿namespace DefaultNamespace;
+
+public class RefrigeratedContainer : Container, IHazardNotifier
+{
+    public string ProductType { get; }
+    public double RequiredTemperature { get; }
+
+    public RefrigeratedContainer(string serialNumber, double height, double tareWeight, double depth, double maxPayload, string productType, double requiredTemperature)
+        : base(serialNumber, height, tareWeight, depth, maxPayload)
+    {
+        ProductType = productType;
+        RequiredTemperature = requiredTemperature;
+    }
+
+    public override void LoadCargo(double cargoMass)
+    {
+        if (cargoMass > MaximumPayload)
+        {
+            throw new OverfillException("Cargo mass exceeds allowable payload.");
+        }
+
+        MassOfCargo = cargoMass;
+    }
+
+    public override void EmptyCargo()
+    {
+        MassOfCargo = 0;
+    }
+
+    public void NotifyHazard(string containerNumber)
+    {
+        Console.WriteLine($"Hazardous situation detected in refrigerated container: {containerNumber}");
+    }
+}
